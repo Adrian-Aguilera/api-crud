f23663c07a28bd285fbe726389d34a6a768daaee
Adrian-Aguilera <cruz74270@gmail.com>
2024-12-25 00:27:05 -0600

    feat: admin panel registrer model

--- api_crud/models.py
+++ api_crud/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.nombre} - {self.apellido}'
--- api_crud/urls.py
+++ api_crud/urls.py
@@ -4,6 +4,6 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = []
 
 router = DefaultRouter()
-router.register(r'crud', crudViewSet, basename='crud')
+router.register(r'usuario', crudViewSet, basename='crud')
 
 urlpatterns += router.urls
